cb28bd2283064f11e0ffe3242554a234d87c033f
[Vanessa D'Antonio] <[yumin.dantonio@gmail.com]>
2023-03-24 11:02:51 +0000

    add footer section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 // React app
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Components
 import { Navbar } from './components/Navbar';
+import { Wrapper } from './components/Wrapper';
+import { Footer } from './components/Footer';
 
-//Style
-import './index.css';
+// Pages
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+
+// Style
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div>
         <Navbar />
-        <p>
-          Keep track of your expenses.
-        </p>
-        
-      </header>
+        <Wrapper>
+          <Routes>
+            <Route path = "/" element = { <Home /> } />
+            <Route path = "about" element = { <About /> } />
+            <Route path = "contact" element = { <Contact /> } />
+          </Routes>
+        </Wrapper>
+        <Footer />
       </div>
+    </Router>
   );
 }
 
